f30ec6928f8589312f6305bd7f44e61a00a7b358
SavvasZoeCen <74330326+SavvasZoeCen@users.noreply.github.com>
2021-06-27 16:33:59 -0700

    v0c0

---
+++ ethereum_query.py
@@ -0,0 +1,50 @@
+from web3 import Web3
+from hexbytes import HexBytes
+
+IP_ADDR='18.188.235.196'
+PORT='8545'
+
+w3 = Web3(Web3.HTTPProvider('http://' + IP_ADDR + ':' + PORT))
+
+if w3.isConnected():
+#     This line will mess with our autograders, but might be useful when debugging
+#     print( "Connected to Ethereum node" )
+else:
+    print( "Failed to connect to Ethereum node!" )
+
+def get_transaction(tx):
+    tx0 = w3.eth.get_transaction(tx)   #YOUR CODE HERE
+    return tx0
+
+# Return the gas price used by a particular transaction,
+#   tx is the transaction
+def get_gas_price(tx):
+    gas_price = tx['gasPrice'] #YOUR CODE HERE
+    return gas_price
+
+def get_gas(tx):
+    gas = tx['gas'] #YOUR CODE HERE
+    return gas
+
+def get_transaction_cost(tx):
+    tx_cost = get_gas_price(tx) * get_gas(tx) #YOUR CODE HERE
+    return tx_cost
+
+def get_block_cost(block_num):
+    block = w3.eth.get_block(block_num)
+    transactions = block['transactions']
+    block_cost = 0  #YOUR CODE HERE
+    for tx in transactions:
+      block_cost += get_transaction_cost(tx)
+    return block_cost
+
+# Return the hash of the most expensive transaction
+def get_most_expensive_transaction(block_num):
+    block = w3.eth.get_block(block_num)
+    transactions = block['transactions']
+    max_tx = 0
+    for tx in transactions:
+      max_tx = max(max_tx, get_transaction_cost(tx))
+    
+    #max_tx = HexBytes('0xf7f4905225c0fde293e2fd3476e97a9c878649dd96eb02c86b86be5b92d826b6')  #YOUR CODE HERE
+    return max_tx
